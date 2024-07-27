b0454e57f1a447955e5fa0f063e0e4019ac0b1fb
Oscar Frias Viñals <140022816+ofvinals@users.noreply.github.com>
2024-07-27 17:15:28 -0300

    cambio fondo oscuro

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -10,23 +10,23 @@ import { FormUser } from '../pages/users/FormUser';
 import { EditProfile } from '../pages/auth/EditProfile';
 
 export function AppRoutes() {
-  return (
-    <Routes>
-      <Route path='/' element={<Home />} />
-      <Route path='/home' element={<Home />} />
-      <Route element={<PublicRoute />}>
-        <Route path='/login' element={<Login />} />
-        <Route path='/register' element={<Register />} />
-      </Route>
-      <Route element={<PrivateRoute allowedRoles={['admin']} />}>
-        <Route path='/users' element={<Users />} />
-        <Route path='/users/upload' element={<FormUser />} />
-        <Route path='/users/upload/:id' element={<FormUser />} />
-      </Route>
-      <Route element={<PrivateRoute allowedRoles={['admin', 'user']} />}>
-        <Route path='/account' element={<MyAccount />} />
-        <Route path='/account/edit' element={<EditProfile />} />
-      </Route>
-    </Routes>
-  );
+	return (
+		<Routes>
+			<Route path='/' element={<Home />} />
+			<Route path='/home' element={<Home />} />
+			<Route element={<PublicRoute />}>
+				<Route path='/login' element={<Login />} />
+				<Route path='/register' element={<Register />} />
+			</Route>
+			<Route element={<PrivateRoute allowedRoles={['admin']} />}>
+				<Route path='/users' element={<Users />} />
+				<Route path='/users/upload' element={<FormUser />} />
+				<Route path='/users/upload/:id' element={<FormUser />} />
+			</Route>
+			<Route element={<PrivateRoute allowedRoles={['admin', 'user']} />}>
+				<Route path='/account' element={<MyAccount />} />
+				<Route path='/account/edit' element={<EditProfile />} />
+			</Route>
+		</Routes>
+	);
 }
