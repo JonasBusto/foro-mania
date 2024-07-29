0dd60dfbba8c149a40faa61b2badc7ed19ce69df
JonasBusto <jonasrex00@gmail.com>
2024-07-29 13:54:25 -0300

    footer modificado v1

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,15 +4,15 @@ import { CategoryMenu } from '../components/home/CategoryMenu';
 import { TagsList } from '../components/home/TagsList';
 
 export const Home = () => {
-	return (
-		<section className='bg-neutral-800'>
-			<Banner />
-			<CategoryMenu />
-			<hr className='m-2 h-[2px] bg-slate-300' />
-			<div className='flex flex-row flex-wrap items-start justify-around w-full pb-5'>
-				<CategoryList />
-				<TagsList />
-			</div>
-		</section>
-	);
+  return (
+    <section>
+      <Banner />
+      <CategoryMenu />
+      <hr className='m-2 h-[2px] bg-slate-300' />
+      <div className='flex flex-row flex-wrap items-start justify-around w-full pb-5'>
+        <CategoryList />
+        <TagsList />
+      </div>
+    </section>
+  );
 };
