1a5db9621079032dc1da419db6afe51dcab8fe57
JonasBusto <jonasrex00@gmail.com>
2024-08-05 23:43:47 -0300

    hashrouter agregado

--- src/main.jsx
+++ src/main.jsx
@@ -1,19 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import 'primeicons/primeicons.css';
 import { App } from './App.jsx';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { store } from './store/index.js';
 import { AppProvider } from './context/AppContext.jsx';
+import 'primeicons/primeicons.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
       <AppProvider>
-        <BrowserRouter>
+        <HashRouter>
           <App />
-        </BrowserRouter>
+        </HashRouter>
       </AppProvider>
     </Provider>
   </React.StrictMode>
