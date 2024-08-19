0ab809e6b5abbfebf4d5048775521f6244371441
githubfacu <elorzfacundo@gmail.com>
2024-08-19 16:59:34 -0300

    home test principales funcionalidades

--- test/pages/home.test.jsx
+++ test/pages/home.test.jsx
@@ -5,6 +5,7 @@ import { Home } from '../../src/pages/Home';
 import { Provider } from 'react-redux';
 import { store } from '../../src/store';
 import { MemoryRouter } from 'react-router-dom';
+import { CategoriesMock } from '../../src/helpers/Categories'
 
 
 describe('Visulizacion del home', () => {
@@ -21,7 +22,18 @@ describe('Visulizacion del home', () => {
 
     vi.mock('../../src/hooks/useLoad', () => ({
         useLoad: () => ({ isLoading: false }),
-    }));
+    }))
+
+    vi.mock('../../src/hooks/useAuth', () => ({
+        useAuth: () => ({ loggedUser: null }), // No hay usuario logueado
+    }))
+
+    vi.mock('../../src/hooks/useCategoryAction', () => ({
+        useCategoryAction: () => ({
+            categories: CategoriesMock
+        })
+    }))
+
 
     test('Mensaje de bienvenida', () => {
 
@@ -34,42 +46,61 @@ describe('Visulizacion del home', () => {
     })
 
 
-    test('Visualizar botón para realizar una nueva publicación', () => {
+    test('Visualizar botón para realizar una nueva publicación', async () => {
 
-        vi.mock('../../src/hooks/useAuth', () => ({
-            useAuth: () => ({ loggedUser: null }), // No hay usuario logueado
-        }));
-    
         render(
             <Provider store={store}>
-            <MemoryRouter>
-                <Home />
-            </MemoryRouter>
+                <MemoryRouter>
+                    <Home />                    
+                </MemoryRouter>
             </Provider>
-        );
-    
-        let nuevaPublicacionButton = screen.findByText('Nueva Publicación')
-        expect(nuevaPublicacionButton).toBeNull
-    
-        cleanup();
-    
+        )
+
+        let nuevaPublicacionButton = screen.queryByRole('Nueva Publicación')
+        expect(nuevaPublicacionButton).toBeNull()
+
+        cleanup()
+
         vi.mock('../../src/hooks/useAuth', () => ({
             useAuth: () => ({
-            loggedUser: { uid: '123', email: 'test@example.com' }, // Usuario logueado
+                loggedUser: { uid: '123', email: 'test@example.com' }, // Usuario logueado
             }),
-        }));
-    
+        }))
+
         render(
             <Provider store={store}>
-            <MemoryRouter>
-                <Home />
-            </MemoryRouter>
+                <MemoryRouter>
+                    <Home />
+                </MemoryRouter>
             </Provider>
-        );
+        )
 
-        nuevaPublicacionButton = screen.getByRole('link', { name: /Nueva Publicación/i })
-        expect(nuevaPublicacionButton).toBeInTheDocument();
-        expect(nuevaPublicacionButton.textContent).toBe('Nueva Publicación');
-    });
+        nuevaPublicacionButton = await screen.findByRole('link', { name: /Nueva Publicación/i })
+        expect(nuevaPublicacionButton).toBeInTheDocument()
+        expect(nuevaPublicacionButton.textContent).toBe('Nueva Publicación')
+    })
+
+
+    test('Render de categorías en etiquetas de navegación', async () => {
+
+        const categoryLinks = await screen.findAllByRole('link')
+
+        const filteredCategoryLinks = categoryLinks.filter(link => 
+            CategoriesMock.some(category => link.textContent.includes(category.title))
+        )
+
+        if (CategoriesMock.length > 6) {
+            expect(filteredCategoryLinks.length).toBe(6)
+        } else {
+            expect(filteredCategoryLinks.length).toBe(CategoriesMock.length)
+        }
+
+        filteredCategoryLinks.forEach(link => {
+            const category = CategoriesMock.find(cat => link.textContent.includes(cat.title))
+            if (category) {
+                expect(link).toHaveAttribute('href', `/topic-list?category=${category.uid}`)
+            }
+        })
+    })
 
 })
